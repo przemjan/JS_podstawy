7a3faecf1f8eced38bb0d0073472e885332f88b4
karlpron <przemjanisz@interia.pl>
2023-01-13 18:09:30 +0100

    Initial commit

---
+++ podstawy/plik.js
@@ -0,0 +1,11 @@
+let firstname = prompt("Jak się nazywasz?","");
+
+
+let lastname = prompt("Nazwisko?", "");
+
+let numerbuta = prompt("Numer buta?", "0");
+numerbuta = parseInt(numerbuta, 10);
+
+document.write("Witaj " + firstname + "!");
+document.write(", " +lastname)
+document.write(", " + numerbuta);
